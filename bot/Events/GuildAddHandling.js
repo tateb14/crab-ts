55f7f9a131b7f2de659fd2d44da06b11a3db908e
tate.b <121896551+TateBasham@users.noreply.github.com>
2025-08-03 21:29:44 -0500

    Improved exclusion features - tate.b
    improvement: Added an embed which will be sent to the owner when Crab leaves a server due to an exclusion.

--- bot/Events/GuildAddHandling.js
+++ bot/Events/GuildAddHandling.js
@@ -9,7 +9,13 @@ module.exports = {
     const GuildExluded = await CrabGuildExclusion.findOne({ crab_guildId: guild.id })
     if (GuildExluded) {
       const user = await guild.fetchOwner()
-      await user.send("Embed here")
+      const ExclusionEmbed = new EmbedBuilder()
+      .setColor(0xF4A261)
+      .setDescription("This message is in regards of the recent exclusion from the service of **Crab**.\n\nTo clarify, an exclusion from our service comes very rarely and is only applied if a server has broken our terms of service or violated a major rule. Exclusions are not appealable, but they can be removed if an Executive Board member choses to.\n\nThe details of your exclusion are confidential and secure to only Crab staff members. If you wish to inquiry about the exclusion, please see our [support](https://discord.gg/8XScx8MNfE).")
+      .setFooter({ text: "Crab Legal Affairs Team" })
+      .setTitle("Crab Exclusion Notice")
+      .setTimestamp()
+      await user.send({ embeds: [ExclusionEmbed] })
       client.guilds.cache.get(guild.id).leave()
       return;
     }
