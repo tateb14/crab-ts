ee997d72e60fb894878191c0ec9021464081fdc6
tate.b <121896551+tateb14@users.noreply.github.com>
2025-08-26 19:51:20 -0500

    Update data.js

--- bot/Commands/Prefix/data.js
+++ bot/Commands/Prefix/data.js
@@ -12,21 +12,22 @@ module.exports = {
   aliases: ["d"],
   execute: async (message, client) => {
     const AuthorizedRoles = ["1350154554175520889", "1397681938629787773"];
-    if (message.guild.id === "1348623820331679744") {
-      if (AuthorizedRoles.some((roleId) => message.member.roles.cache.has(roleId))) {
-        const args = message.content.trim().split(/ +/);
-        const ID = args.slice(1).join(" ").toLowerCase();
-        const result = await detectIdType(client, ID)
-        if (result.type === "user") {
-          message.reply("User")
-        } else if (result.type === 'guild') {
-          message.reply(`It's a guild: ${result.object.name}`);
-        } else {
-          message.reply("Invalid ID");
-        }
-        const embed = new EmbedBuilder()
-        .setColor(0xec3935)
-      }
-    }
+    message.reply("Hi!")
+    // if (message.guild.id === "1348623820331679744") {
+    //   if (AuthorizedRoles.some((roleId) => message.member.roles.cache.has(roleId))) {
+    //     const args = message.content.trim().split(/ +/);
+    //     const ID = args.slice(1).join(" ").toLowerCase();
+    //     const result = await detectIdType(client, ID)
+    //     if (result.type === "user") {
+    //       message.reply("User")
+    //     } else if (result.type === 'guild') {
+    //       message.reply(`It's a guild: ${result.object.name}`);
+    //     } else {
+    //       message.reply("Invalid ID");
+    //     }
+    //     const embed = new EmbedBuilder()
+    //     .setColor(0xec3935)
+    //   }
+    // }
   },
 };
