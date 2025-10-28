542f7f2037112ded6fc53513176d0f1f9ca80204
tate.b <121896551+tateb14@users.noreply.github.com>
2025-10-27 21:09:07 -0500

    Removed .js and other unnecessary files

--- bot/Functions/capitalizeFirstLetter.js
+++
@@ -1,6 +0,0 @@
-function capitalizeFirstLetter(str) {
-  if (!str) return str;
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-module.exports = capitalizeFirstLetter
--- bot/Functions/randomId.js
+++
@@ -1,6 +0,0 @@
-function randomString(length, chars) {
-  var result = '';
-  for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-  return result;
-}
-module.exports = randomString
