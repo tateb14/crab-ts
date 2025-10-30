b5c1ce98a355d10ae2983844afef7f6520b11744
tate.b <121896551+tateb14@users.noreply.github.com>
2025-10-30 15:37:46 -0500

    Delete dist directory

--- dist/Model/crab-user-flags.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const crabUserFlagsSchema = new mongoose_1.default.Schema({
-    userID: { type: String, required: false },
-    flags: { type: [String], required: false },
-});
-const crabUserFlags = mongoose_1.default.model("User-Flags", crabUserFlagsSchema);
-exports.default = crabUserFlags;
