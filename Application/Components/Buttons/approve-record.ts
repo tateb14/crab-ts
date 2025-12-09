9345145293a3f924bd112b4b7c09301655123fea
tate.b <121896551+tateb14@users.noreply.github.com>
2025-12-09 11:56:25 -0600

    converted approve-record.ts

--- Application/Components/Buttons/approve-record.ts
+++ Application/Components/Buttons/approve-record.ts
@@ -1,72 +1,64 @@
-import { EmbedBuilder, inlineCode, ButtonBuilder, ButtonStyle, ActionRowBuilder, MessageFlags, ButtonInteraction, Client, GuildMember, } from "discord.js";
+import { EmbedBuilder, inlineCode, ButtonBuilder, ButtonStyle, ActionRowBuilder, MessageFlags, ButtonInteraction, Client, GuildMember, User, Guild } from "discord.js";
 
-const CrabConfig = require("../../schemas/CrabConfig");
-const GuildRecord = require("../../schemas/GuildRecord");
+import crabConfig from "../../Models/crab-config";
+import guildRecord from "../../Models/guild-record";
+import * as emojis from "../../../emojis.json";
 
-module.exports = {
-  customId: "crab-button_record-approve",
-  execute: async (interaction: ButtonInteraction, client: Client) => {
-    const guild = interaction.guild;
-    const member = interaction.member as GuildMember;
-    
-    if (!guild || !(member instanceof GuildMember))
-    {
-        return;
-    }
+export default {
+    customId: "crab-button_record-approve",
+    execute: async (interaction: ButtonInteraction, client: Client) => {
+        const guild = interaction.guild as Guild
+        const member = interaction.member as GuildMember;
+        const user = interaction.user as User;
+        if (!guild || !member || !user || user.bot) return;
 
-    const guildConfig = await CrabConfig.findOne(
-    {
-      guildId: interaction.guild.id,
-    });
-
-    const supervisorRole = guildConfig.perms_SupervisorRole;
-    const hiCommRole = guildConfig.perms_HiCommRole;
-    const aARole = guildConfig.perms_AllAccessRole;
-
-    if (member.roles.cache.has(supervisorRole) || member.roles.cache.has(hiCommRole) || member.roles.cache.has(aARole)) 
-    {
-      const Record = await GuildRecord.findOneAndUpdate(
-        { messageId: interaction.message.id }, { $set: { reviewedBy: interaction.user.id } }, { new: true }
-      );
-      
-      const embed = interaction.message.embeds[0];
-      const user = await interaction.guild.members.fetch(Record.issuedBy);
-      const approvedEmbed = EmbedBuilder.from(embed);
-      approvedEmbed.setColor(0x2a9d8f);
-
-      const serverButton = new ButtonBuilder()
-        .setCustomId("crab-button_server-name-disabled")
-        .setDisabled(true)
-        .setStyle(ButtonStyle.Secondary)
-        .setLabel(`Sent from ${interaction.guild.name}`);
-
-      const row = new ActionRowBuilder().addComponents(serverButton);
-
-      interaction.update({
-        content: `This record has been approved by ${interaction.user}`,
-        embeds: [approvedEmbed],
-        components: [],
-      });
-      if (user) {
-        try {
-          await user.send({
-            content: `**Record ID:** ${inlineCode(
-              Record.id
-            )} has been approved by ${interaction.user}`,
-            components: [row],
-          });
-        } catch (err) {
-          return interaction.followUp({
-            content: "I could not DM this user.",
-            flags: MessageFlags.Ephemeral,
-          });
+        const guildConfig = await crabConfig.findOne({
+            guildId: guild.id,
+        });
+        // ! Check guild config
+        if (!guildConfig) {
+            return interaction.reply({ content: `**@${user.username}**, no guild configuration was found. Please contact Tropical Systems.`, flags: MessageFlags.Ephemeral });
         }
-      }
-    } else {
-      interaction.reply({
-        content: "**Insufficient** permissions.",
-        flags: MessageFlags.Ephemeral,
-      });
-    }
-  },
+        //? Fetch all role ids
+        const personnelRoleId = guildConfig!.perms_PersonnelRole;
+        const supervisorRoleId = guildConfig!.perms_SupervisorRole;
+        const highCommRoleId = guildConfig!.perms_HiCommRole;
+        const allAccessRoleId = guildConfig!.perms_AllAccessRole;
+        if (!personnelRoleId || !supervisorRoleId || !highCommRoleId || !allAccessRoleId) {
+            return interaction.reply({ content: `${emojis.x}, this server has not setup all permission roles.`, flags: MessageFlags.Ephemeral });
+        }
+        if (!member.roles.cache.hasAny(personnelRoleId, supervisorRoleId, highCommRoleId, allAccessRoleId)) {
+            return interaction.reply({ content: `${emojis.x}, **@${user.username}**, you cannot use this command.`, flags: MessageFlags.Ephemeral });
+        }
+        const record = await guildRecord.findOneAndUpdate({ messageId: interaction.message.id }, { $set: { reviewedBy: interaction.user.id } }, { new: true });
+
+        if (!record) {
+            return interaction.reply({ content: `${emojis.x}, **@${user.username}**, no record was found with this id string, please input a different id.`, flags: MessageFlags.Ephemeral });
+        }
+
+        const embed = interaction.message.embeds[0];
+        const recordIssuer = await guild.members.fetch(record.issuedBy);
+        const approvedEmbed = EmbedBuilder.from(embed);
+        approvedEmbed.setColor(0x2a9d8f);
+
+        const serverButton = new ButtonBuilder().setCustomId("crab-button_server-name-disabled").setDisabled(true).setStyle(ButtonStyle.Secondary).setLabel(`Sent from ${guild.name}`);
+
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(serverButton);
+
+        interaction.update({
+            content: `This record has been approved by ${interaction.user}`,
+            embeds: [approvedEmbed],
+            components: [],
+        });
+        if (recordIssuer) {
+            try {
+                await recordIssuer.send({
+                    content: `**Record ID:** ${inlineCode(record.id)} has been approved by ${interaction.user}`,
+                    components: [row],
+                });
+            } catch (err) {
+                return interaction.followUp({ content: `${emojis.x}, **@${user.username}**, I could not direct message this user.`, flags: MessageFlags.Ephemeral });
+            }
+        }
+    },
 };
